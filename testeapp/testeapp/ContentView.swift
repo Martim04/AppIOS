c9e42eeeb587617d1f5f8fd5282dcdbdaf0b33ee
Martin <martim1243@gmail.com>
2024-04-27 02:07:20 +0100

    Fix Bugs and ui aspects

--- testeapp/testeapp/ContentView.swift
+++ testeapp/testeapp/ContentView.swift
@@ -15,31 +15,59 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
-            HStack{
-                ForEach(0..<cardCount, id: \.self){index in
-                    CardView(content: emojis[index])
-                    
-                }
-            }
-            .foregroundColor(.yellow)
-
-                HStack{
-                    
-                    Button("Remove card"){
-                        cardCount -= 1
-                    }
-                    Spacer()
-                    Button("Add card"){
-                        
-                        cardCount += 1
-                    }
-                }
+            ScrollView{
+                cards
             }
             
-            .padding()
+            Spacer()
+            cardCountAdjusters
             
         }
+        .padding()
     }
+
+    var cards: some View{
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
+            ForEach(0..<cardCount, id: \.self){index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+                
+            }
+            .foregroundColor(.yellow)
+        }
+    }
+    
+    
+    var cardCountAdjusters: some View{
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdd
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+       
+    }
+    
+        
+    func CardCountAdjuster(by offset: Int, symbol: String) -> some View{
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount	+ offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View{
+        return CardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    
+    var cardAdd: some View{
+        return CardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+}
 
 struct CardView: View {
     let content: String
@@ -48,15 +76,14 @@ struct CardView: View {
     var body: some View {
         ZStack{
             let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp{
+            Group{
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 4)
                 Text(content).font(.largeTitle)
-            }else{
-                base.fill()
-                
             }
-            
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0: 1 )
+
         }
         .onTapGesture {
             isFaceUp.toggle()
